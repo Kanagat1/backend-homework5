684b63e3392599865b9f3a6fa03191ceae60e670
Kanagat Bekmaganbetov <bekkananur@gmail.com>
2025-03-23 16:24:59 +0500

    backend-homework6

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import cors from "cors"
 
 const app = express();
 const PORT = 3001;
-// const connectionString = "mongodb://127.0.0.1:27017/homework"
  
 app.use(express.json()); // Middleware для обработки JSON
 app.use(cors())
@@ -14,18 +13,6 @@ app.use(cors())
 app.use("/api", libraryRoutes);
 app.use("/auth", authRoutes);
 
-// Подключение к MongoDB
-// async function connectDatabase() {
-//     try {
-//     await mongoose.connect(connectionString);
-//     console.log("Connected to MongoDB");
-        
-//     } catch (error) {
-//         console.log("Failed to connect to MongoDB:", error.message);
-//     }
-  
-    
-// }
 
   
     app.listen(PORT, async() => {
